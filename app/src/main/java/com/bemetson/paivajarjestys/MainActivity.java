75c574efd3954f62499876c7d7733152a1464712
Henri <loran.duong@hotmail.com>
2017-08-09 18:37:30 +0300

    Updated toolbar to fit overflowmenu. Updated logic for bottom navigation.

--- app/src/main/java/com/bemetson/paivajarjestys/MainActivity.java
+++ app/src/main/java/com/bemetson/paivajarjestys/MainActivity.java
@@ -4,13 +4,17 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -29,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        //toolbar.setTitleTextColor(Color.WHITE);
+        //toolbar.setSubtitleTextColor(Color.WHITE);
+
         scrollview = (ScrollView) findViewById(R.id.main_scrollview);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -56,6 +66,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.w("Menu", "Menu clicked");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -68,14 +101,18 @@ public class MainActivity extends AppCompatActivity {
             );
             params.addRule(RelativeLayout.ABOVE, R.id.navigation);
             params.setMargins(12, 12, 12, 12);
+
+            LinearLayout weekdays = (LinearLayout) findViewById(R.id.weekdays);
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                    // params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
                     //scrollview.setLayoutParams(params);
+                    weekdays.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_dashboard:
                     //params.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
                     //scrollview.setLayoutParams(params);
+                    weekdays.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
